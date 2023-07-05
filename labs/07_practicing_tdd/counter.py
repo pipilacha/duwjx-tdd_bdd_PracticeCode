53212394c96bb6bd10b75fadcedfdab50e5ffb52
mauVR <44210264+mauVR@users.noreply.github.com>
2023-07-04 22:52:02 -0600

    finished counter service

--- labs/07_practicing_tdd/counter.py
+++ labs/07_practicing_tdd/counter.py
@@ -12,7 +12,36 @@ def create_counter(name):
     global COUNTERS
 
     if name in COUNTERS:
-        return {"message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
     COUNTERS[name] = 0
     return { name: COUNTERS[name] }, status.HTTP_201_CREATED
+
+@app.route("/counters/<name>", methods=["GET"])
+def get_counter(name):
+    """Gets a counter"""
+    app.logger.info(f"Request to get counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        return { name: COUNTERS[name] }, status.HTTP_200_OK
+    return {"message": f"Counter {name} does not exists"}, status.HTTP_404_NOT_FOUND
+
+@app.route("/counters/<name>", methods=["PUT"])
+def increment_counter(name):
+    """Updates a counter"""
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        COUNTERS[name] += 1
+        return { name: COUNTERS[name] }, status.HTTP_200_OK
+    return {"message": f"Counter {name} does not exists"}, status.HTTP_404_NOT_FOUND
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Deletes a counter"""
+    app.logger.info(f"Request to delete counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        del COUNTERS[name]
+        return '',status.HTTP_204_NO_CONTENT
+    return {"message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
--- labs/07_practicing_tdd/test_counter.py
+++ labs/07_practicing_tdd/test_counter.py
@@ -3,21 +3,23 @@ Test Cases for Counter Web Service
 """
 from unittest import TestCase
 import status
-from counter import app
+from counter import app, COUNTERS
 
 class CounterTest(TestCase):
     """Test Cases for Counter Web Service"""
 
     def setUp(self):
         self.client = app.test_client()
+        COUNTERS.clear()
 
     def test_create_a_counter(self):
         """It should create a counter"""
-        result = self.client.post("/counters/foo")
+        counter = 'foo'
+        result = self.client.post(f"/counters/{counter}")
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
         data = result.get_json()
-        self.assertIn("foo", data)
-        self.assertEqual(data["foo"], 0)
+        self.assertIn(counter, data)
+        self.assertEqual(data[counter], 0)
 
     def test_duplicate_counter(self):
         """It should return an error for duplicates"""
@@ -25,3 +27,59 @@ class CounterTest(TestCase):
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
         result = self.client.post("/counters/bar")
         self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_get_counter(self):
+        """It should return a counter and its value"""
+        counter = 'foo'
+        result = self.client.post(f"/counters/{counter}")
+        result = self.client.get(f"/counters/{counter}")
+        data = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertIn(counter, data)
+        self.assertEqual(data[counter], 0)
+    def test_get_nonexisting_counter(self):
+        """It should return an error counter does not exist"""
+        counter = 'foo'
+        result = self.client.get(f"/counters/{counter}")
+        data = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIn('message', data)
+        self.assertEqual(data['message'], f"Counter {counter} does not exists")
+
+    def test_update_counter(self):
+        """It should increment the counter by one"""
+        counter = 'foo'
+        result = self.client.post(f"/counters/{counter}")
+        data = result.get_json()
+        self.assertEqual(data[counter], 0)
+        result = self.client.put(f"/counters/{counter}")
+        data = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertIn(counter, data)
+        self.assertEqual(data[counter], 1)
+
+    def test_update_nonexistent_counter(self):
+        """It should return an error counter does not exist"""
+        counter = 'foo'
+        result = self.client.put(f"/counters/{counter}")
+        data = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIn('message', data)
+        self.assertEqual(data['message'], f"Counter {counter} does not exists")
+
+    def test_delete_counter(self):
+        """It should return a counter deleted message"""
+        counter = 'foo'
+        result = self.client.post(f"/counters/{counter}")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.delete(f"/counters/{counter}")
+        data = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_204_NO_CONTENT)
+    def test_delete_nonexisting_counter(self):
+        """It should return a counter deleted message"""
+        counter = 'foo'
+        result = self.client.delete(f"/counters/{counter}")
+        data = result.get_json()
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIn('message', data)
+        self.assertEqual(data['message'], f"Counter {counter} does not exist")
